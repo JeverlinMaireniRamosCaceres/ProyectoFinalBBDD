bf855975766e95e2b58169cdfa0d003f9e6dcc13
ErwinMH <niwredev@gmail.com>
2024-11-21 09:05:24 -0400

    Agregando atributos a la clase HistoriaClinica

--- ProyectoFinal/src/logico/HistoriaClinica.java
+++ ProyectoFinal/src/logico/HistoriaClinica.java
@@ -1,5 +1,13 @@
 package logico;
 
-public class HistoriaClinica {
+import java.util.ArrayList;
+import java.util.Date;
 
+public class HistoriaClinica {
+	int idHistoriaClinica;
+	String etnia;
+	String antecedentes;
+	Date fecha;
+	ArrayList<Enfermedad> lasEnfermedades;
+	ArrayList<Consulta> lasConsultas;
 }
