d09082fca0a09beb9e7c148f6f5b9737159182ad
ErwinMH <niwredev@gmail.com>
2024-11-21 09:36:13 -0400

    Agregando Getters y Setters a la clase ClinicaMedica

--- ProyectoFinal/src/logico/ClinicaMedica.java
+++ ProyectoFinal/src/logico/ClinicaMedica.java
@@ -5,4 +5,17 @@ import java.util.ArrayList;
 public class ClinicaMedica {
 	ArrayList<Persona> lasPersonas;
 	ArrayList<Consulta> lasConsultas;
+	
+	public ArrayList<Persona> getLasPersonas() {
+		return lasPersonas;
+	}
+	public void setLasPersonas(ArrayList<Persona> lasPersonas) {
+		this.lasPersonas = lasPersonas;
+	}
+	public ArrayList<Consulta> getLasConsultas() {
+		return lasConsultas;
+	}
+	public void setLasConsultas(ArrayList<Consulta> lasConsultas) {
+		this.lasConsultas = lasConsultas;
+	}
 }
