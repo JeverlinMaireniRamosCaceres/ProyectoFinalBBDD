138cf08e5a27953a78e46873d383db831bd1926f
ErwinMH <niwredev@gmail.com>
2024-11-21 09:39:07 -0400

    Agregando Getters y Setters a la clase Consulta

--- ProyectoFinal/src/logico/Consulta.java
+++ ProyectoFinal/src/logico/Consulta.java
@@ -9,4 +9,41 @@ public class Consulta {
 	String motivo;
 	String diagnostico;
 	String indicacion;
+	
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public Medico getMedico() {
+		return medico;
+	}
+	public void setMedico(Medico medico) {
+		this.medico = medico;
+	}
+	public Paciente getPaciente() {
+		return paciente;
+	}
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+	public String getMotivo() {
+		return motivo;
+	}
+	public void setMotivo(String motivo) {
+		this.motivo = motivo;
+	}
+	public String getDiagnostico() {
+		return diagnostico;
+	}
+	public void setDiagnostico(String diagnostico) {
+		this.diagnostico = diagnostico;
+	}
+	public String getIndicacion() {
+		return indicacion;
+	}
+	public void setIndicacion(String indicacion) {
+		this.indicacion = indicacion;
+	}
 }
