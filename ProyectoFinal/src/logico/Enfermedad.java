7c2cbf2cc7301d43b184829cd5ba01de6d0c5320
ErwinMH <niwredev@gmail.com>
2024-11-21 09:32:20 -0400

    Agregando Getters y Setters a la clase Enfermedad

--- ProyectoFinal/src/logico/Enfermedad.java
+++ ProyectoFinal/src/logico/Enfermedad.java
@@ -5,4 +5,31 @@ public class Enfermedad {
 	String nombre;
 	String sintomas;
 	String tipo;
+	
+	public int getIdEnfermedad() {
+		return idEnfermedad;
+	}
+	public void setIdEnfermedad(int idEnfermedad) {
+		this.idEnfermedad = idEnfermedad;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getSintomas() {
+		return sintomas;
+	}
+	public void setSintomas(String sintomas) {
+		this.sintomas = sintomas;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
 }
