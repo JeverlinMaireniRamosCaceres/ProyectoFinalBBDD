a659ab34404fe6342efe8814d63cc87879b4f79f
ErwinMH <niwredev@gmail.com>
2024-11-21 09:34:06 -0400

    Agregando Getters y Setters a la clase HistoriaClinica

--- ProyectoFinal/src/logico/HistoriaClinica.java
+++ ProyectoFinal/src/logico/HistoriaClinica.java
@@ -10,4 +10,41 @@ public class HistoriaClinica {
 	Date fecha;
 	ArrayList<Enfermedad> lasEnfermedades;
 	ArrayList<Consulta> lasConsultas;
+	
+	public int getIdHistoriaClinica() {
+		return idHistoriaClinica;
+	}
+	public void setIdHistoriaClinica(int idHistoriaClinica) {
+		this.idHistoriaClinica = idHistoriaClinica;
+	}
+	public String getEtnia() {
+		return etnia;
+	}
+	public void setEtnia(String etnia) {
+		this.etnia = etnia;
+	}
+	public String getAntecedentes() {
+		return antecedentes;
+	}
+	public void setAntecedentes(String antecedentes) {
+		this.antecedentes = antecedentes;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public ArrayList<Enfermedad> getLasEnfermedades() {
+		return lasEnfermedades;
+	}
+	public void setLasEnfermedades(ArrayList<Enfermedad> lasEnfermedades) {
+		this.lasEnfermedades = lasEnfermedades;
+	}
+	public ArrayList<Consulta> getLasConsultas() {
+		return lasConsultas;
+	}
+	public void setLasConsultas(ArrayList<Consulta> lasConsultas) {
+		this.lasConsultas = lasConsultas;
+	}
 }
