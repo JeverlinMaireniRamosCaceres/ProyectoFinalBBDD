5b622b6fc48af6ef4e13a297287b5a1c1b26a074
ErwinMH <niwredev@gmail.com>
2024-11-21 09:40:38 -0400

    Agregando Getters y Setters a la clase Vacuna

--- ProyectoFinal/src/logico/Vacuna.java
+++ ProyectoFinal/src/logico/Vacuna.java
@@ -7,4 +7,29 @@ public class Vacuna {
 	Date fecha;
 	String nombreVacuna;
 	String tipo;
+	
+	public int getIdVacuna() {
+		return idVacuna;
+	}
+	public void setIdVacuna(int idVacuna) {
+		this.idVacuna = idVacuna;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public String getNombreVacuna() {
+		return nombreVacuna;
+	}
+	public void setNombreVacuna(String nombreVacuna) {
+		this.nombreVacuna = nombreVacuna;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
 }
