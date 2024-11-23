6764d785546aa19b3de63edcb15c0d003df21be6
jever <jever@Jeverlin>
2024-11-23 08:48:54 -0400

    Correcciones de atributos entre clase Cita y Consulta

--- ProyectoFinal/src/logico/Cita.java
+++ ProyectoFinal/src/logico/Cita.java
@@ -4,32 +4,23 @@ import java.util.Date;
 
 public class Cita {
 
-	private Paciente paciente;
+	private String nombrePersona;
 	private Medico medico;
 	private Date fecha;
-	private String diagnostico;
-	private String tratamiento;
-	private boolean tratado;
-	private boolean importante;
 	
-	public Cita(Paciente paciente, Medico medico, Date fecha, String diagnostico, String tratamiento, boolean tratado,
-			boolean importante) {
+	public Cita(String nombrePersona, Medico medico, Date fecha) {
 		super();
-		this.paciente = paciente;
+		this.nombrePersona = nombrePersona;
 		this.medico = medico;
 		this.fecha = fecha;
-		this.diagnostico = diagnostico;
-		this.tratamiento = tratamiento;
-		this.tratado = tratado;
-		this.importante = importante;
 	}
 
-	public Paciente getPaciente() {
-		return paciente;
+	public String getNombrePersona() {
+		return nombrePersona;
 	}
 
-	public void setPaciente(Paciente paciente) {
-		this.paciente = paciente;
+	public void setNombrePersona(String nombrePersona) {
+		this.nombrePersona = nombrePersona;
 	}
 
 	public Medico getMedico() {
@@ -47,39 +38,5 @@ public class Cita {
 	public void setFecha(Date fecha) {
 		this.fecha = fecha;
 	}
-
-	public String getDiagnostico() {
-		return diagnostico;
-	}
-
-	public void setDiagnostico(String diagnostico) {
-		this.diagnostico = diagnostico;
-	}
-
-	public String getTratamiento() {
-		return tratamiento;
-	}
-
-	public void setTratamiento(String tratamiento) {
-		this.tratamiento = tratamiento;
-	}
-
-	public boolean isTratado() {
-		return tratado;
-	}
-
-	public void setTratado(boolean tratado) {
-		this.tratado = tratado;
-	}
-
-	public boolean isImportante() {
-		return importante;
-	}
-
-	public void setImportante(boolean importante) {
-		this.importante = importante;
-	}
-	
-	
 	
 }
--- ProyectoFinal/src/logico/Consulta.java
+++ ProyectoFinal/src/logico/Consulta.java
@@ -4,58 +4,94 @@ import java.util.Date;
 
 public class Consulta {
 	
+	private int idConsulta;
 	private Date fecha;
 	private Medico medico;
 	private Paciente paciente;
 	private String motivo;
 	private String diagnostico;
 	private String indicacion;
+	private boolean importante;
 	
-	public Consulta(Date fecha, Medico medico, Paciente paciente, String motivo, String diagnostico,
-			String indicacion) {
+	public Consulta(int idConsulta, Date fecha, Medico medico, Paciente paciente, String motivo, String diagnostico,
+			String indicacion, boolean importante) {
 		super();
+		this.idConsulta = idConsulta;
 		this.fecha = fecha;
 		this.medico = medico;
 		this.paciente = paciente;
 		this.motivo = motivo;
 		this.diagnostico = diagnostico;
 		this.indicacion = indicacion;
+		this.importante = importante;
 	}
-	
+
+	public int getIdConsulta() {
+		return idConsulta;
+	}
+
+	public void setIdConsulta(int idConsulta) {
+		this.idConsulta = idConsulta;
+	}
+
 	public Date getFecha() {
 		return fecha;
 	}
+
 	public void setFecha(Date fecha) {
 		this.fecha = fecha;
 	}
+
 	public Medico getMedico() {
 		return medico;
 	}
+
 	public void setMedico(Medico medico) {
 		this.medico = medico;
 	}
+
 	public Paciente getPaciente() {
 		return paciente;
 	}
+
 	public void setPaciente(Paciente paciente) {
 		this.paciente = paciente;
 	}
+
 	public String getMotivo() {
 		return motivo;
 	}
+
 	public void setMotivo(String motivo) {
 		this.motivo = motivo;
 	}
+
 	public String getDiagnostico() {
 		return diagnostico;
 	}
+
 	public void setDiagnostico(String diagnostico) {
 		this.diagnostico = diagnostico;
 	}
+
 	public String getIndicacion() {
 		return indicacion;
 	}
+
 	public void setIndicacion(String indicacion) {
 		this.indicacion = indicacion;
 	}
+
+	public boolean isImportante() {
+		return importante;
+	}
+
+	public void setImportante(boolean importante) {
+		this.importante = importante;
+	}
+	
+	
+	
+	
+	
 }
